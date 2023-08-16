df85435f44d63ce907c25180e8f5f513a59b096c
Mabel <mabelsanromanf@gmail.com>
2023-08-16 13:56:30 +0200

    subida home/register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, //para que home sea la que aparezca por defecto
+  {path: "home", component: HomeComponent},
+  {path: "register", component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
